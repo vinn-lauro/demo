70bb9926c3b3666994d1ee8cb52cbc015fb05dd3
Vinn <vinnlauro@gmail.com>
2025-08-11 21:34:32 +0800

    factory method pattern

---
+++ factory-method-pattern/Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Demo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFactory factoryA = new ConcreteFactoryA();
+        AbstractProduct productA = factoryA.CreateProduct();
+
+        IFactory factoryB = new ConcreteFactoryB();
+        AbstractProduct productB = factoryB.CreateProduct();
+    }
+}
+
+abstract class AbstractProduct
+{
+    protected AbstractProduct()
+    {
+        Console.WriteLine($"{GetType()} Created!");
+    }
+}
+
+class ConcreteProductA : AbstractProduct { }
+class ConcreteProductB : AbstractProduct { }
+
+interface IFactory
+{
+    AbstractProduct CreateProduct();
+}
+
+class ConcreteFactoryA : IFactory
+{
+    public AbstractProduct CreateProduct() => new ConcreteProductA();
+}
+
+class ConcreteFactoryB : IFactory
+{
+    public AbstractProduct CreateProduct() => new ConcreteProductB();
+}
