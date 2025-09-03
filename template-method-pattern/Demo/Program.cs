3fc61faa5174b1659dec6c9a9b912136ea8e7452
Vinn <vinnlauro@gmail.com>
2025-09-03 09:45:37 +0800

    template method pattern

---
+++ template-method-pattern/Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Demo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AbstractTemplate a = new ConcreteTemplateA();
+        a.Sequence();
+
+        AbstractTemplate b = new ConcreteTemplateB();
+        b.Sequence();
+    }
+}
+
+abstract class AbstractTemplate
+{
+    public void Sequence()
+    {
+        Start();
+        Process();
+        Exit();
+    }
+
+
+    public abstract void Process();
+    public void Start() => Console.WriteLine("Starting Application...");
+    public void Exit() => Console.WriteLine("Exiting Application...");
+}
+
+class ConcreteTemplateA : AbstractTemplate
+{
+    public override void Process() => Console.WriteLine("Starting Concrete Template A Process...");
+}
+
+class ConcreteTemplateB : AbstractTemplate
+{
+    public override void Process() => Console.WriteLine("Starting Concrete Template B Process...");
+}
